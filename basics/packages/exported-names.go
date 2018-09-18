d160d37b54698677c78dffe215f662bfba75a2f0
Krishna V <krishnarules@gmail.com>
2018-09-18 03:06:43 +0000

    basic go files

---
+++ basics/packages/exported-names.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	//exported names start with a capital letter, so below does not run
+	//fmt.Println(math.pi)
+	fmt.Println(math.Pi)
+}
